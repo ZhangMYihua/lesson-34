92eac69a4d0dcbf90a5ac060f948abf579211bd3
Yihua Zhang <michaelzhang2@msn.com>
2019-06-29 07:09:56 -0400

    simplifying our serverjs code into one file

--- server.js
+++ server.js
@@ -2,22 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
-
-const configureRoutes = require('./routes');
-
-configureRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -30,4 +26,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
