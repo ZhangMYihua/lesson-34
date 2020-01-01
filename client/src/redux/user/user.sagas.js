4d6cfd4c2d160f38e2ea1af4a36ec702af803daf
Yihua Zhang <yihuazhang2@gmail.com>
2020-01-01 08:52:14 -0800

    Update user.sagas.js

--- client/src/redux/user/user.sagas.js
+++ client/src/redux/user/user.sagas.js
@@ -110,7 +110,7 @@ export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
-    call(isUserAuthenticated),
+    call(onCheckUserSession),
     call(onSignOutStart),
     call(onSignUpStart),
     call(onSignUpSuccess)
