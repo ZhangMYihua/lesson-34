4b281ad9f7311b5f88cc86fddb5346f8318a8f95
kimiimac <kimi0230@gmail.com>
2021-12-01 21:46:46 +0800

    fit: includes() will return false.  Searching an array of objects should use some() function

--- client/src/redux/cart/cart.reducer.test.js
+++ client/src/redux/cart/cart.reducer.test.js
@@ -1,89 +1,92 @@
-import CartActionTypes from './cart.types';
-import cartReducer from './cart.reducer';
+import CartActionTypes from "./cart.types";
+import cartReducer from "./cart.reducer";
 
 const initialState = {
   hidden: true,
-  cartItems: []
+  cartItems: [],
 };
 
-describe('cartReducer', () => {
-  it('should return initial state', () => {
+describe("cartReducer", () => {
+  it("should return initial state", () => {
     expect(cartReducer(undefined, {})).toEqual(initialState);
   });
 
-  it('should toggle hidden with toggleHidden action', () => {
+  it("should toggle hidden with toggleHidden action", () => {
     expect(
       cartReducer(initialState, { type: CartActionTypes.TOGGLE_CART_HIDDEN })
         .hidden
     ).toBe(false);
   });
 
-  it('should increase quantity of matching item by 1 if addItem action fired with same item as payload', () => {
+  it("should increase quantity of matching item by 1 if addItem action fired with same item as payload", () => {
     const mockItem = {
       id: 1,
-      quantity: 3
+      quantity: 3,
     };
 
     const mockPrevState = {
       hidden: true,
-      cartItems: [mockItem, { id: 2, quantity: 1 }]
+      cartItems: [mockItem, { id: 2, quantity: 1 }],
     };
 
     expect(
       cartReducer(mockPrevState, {
         type: CartActionTypes.ADD_ITEM,
-        payload: mockItem
+        payload: mockItem,
       }).cartItems[0].quantity
     ).toBe(4);
   });
 
-  it('should decrease quantity of matching item by 1 if removeItem action fired with same item as payload', () => {
+  it("should decrease quantity of matching item by 1 if removeItem action fired with same item as payload", () => {
     const mockItem = {
       id: 1,
-      quantity: 3
+      quantity: 3,
     };
 
     const mockPrevState = {
       hidden: true,
-      cartItems: [mockItem, { id: 2, quantity: 1 }]
+      cartItems: [mockItem, { id: 2, quantity: 1 }],
     };
 
     expect(
       cartReducer(mockPrevState, {
         type: CartActionTypes.REMOVE_ITEM,
-        payload: mockItem
+        payload: mockItem,
       }).cartItems[0].quantity
     ).toBe(2);
   });
 
-  it('should remove item from cart if clearItemFromCart action fired with payload of existing item', () => {
+  it("should remove item from cart if clearItemFromCart action fired with payload of existing item", () => {
     const mockItem = {
       id: 1,
-      quantity: 3
+      quantity: 3,
     };
 
     const mockPrevState = {
       hidden: true,
-      cartItems: [mockItem, { id: 2, quantity: 1 }]
+      cartItems: [mockItem, { id: 2, quantity: 1 }],
     };
 
     expect(
       cartReducer(mockPrevState, {
         type: CartActionTypes.CLEAR_ITEM_FROM_CART,
-        payload: mockItem
-      }).cartItems.includes(item => item.id === 1)
+        payload: mockItem,
+      }).cartItems.some((item) => item.id === 1)
     ).toBe(false);
   });
 
-  it('should clear cart if clearCart action fired', () => {
+  it("should clear cart if clearCart action fired", () => {
     const mockPrevState = {
       hidden: true,
-      cartItems: [{ id: 1, quantity: 3 }, { id: 2, quantity: 1 }]
+      cartItems: [
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 1 },
+      ],
     };
 
     expect(
       cartReducer(mockPrevState, {
-        type: CartActionTypes.CLEAR_CART
+        type: CartActionTypes.CLEAR_CART,
       }).cartItems.length
     ).toBe(0);
   });
